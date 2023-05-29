6ae07e3bfc82c01cf91f9dd96bd0458b6b08a48f
Ishaya Solomon <ishaya808@gmail.com>
2023-05-29 13:35:20 +0100

    add files

---
+++ 0x04-React_inline_styling/task_4/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import closeIcon from '../Assests/close-icon.png';
+import NotificationItem from './NotificationItem';
+import PropTypes from 'prop-types';
+import NotificationItemShape from './NotificationItemShape';
+import { StyleSheet, css } from 'aphrodite';
+
+const fadeInAnimation = {
+    '0%': {
+        opacity: 0.5,
+    },
+    '100%': {
+        opacity: 1,
+    },
+};
+
+const bounceAnimation = {
+    '0%': {
+        transform: 'translateY(0px)',
+    },
+    '50%': {
+        transform: 'translateY(-5px)',
+    },
+    '100%': {
+        transform: 'translateY(5px)',
+    },
+};
+
+const styles = StyleSheet.create({
+    Notifications: {
+        padding: '2em',
+        border: '2px dashed red',
+    },
+    menuItem: {
+        textAlign: 'right',
+        float: 'right',
+        backgroundColor: '#fff8f8',
+        cursor: 'pointer',
+        ':hover': {
+            animationName: [fadeInAnimation, bounceAnimation],
+            animationDuration: '1s, 0.5s',
+            animationIterationCount: '3',
+        },
+    },
+    'notification-header': {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    'flex-area': {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+    },
+});
+
+class Notifications extends React.Component {
+    constructor(props) {
+        super(props);
+        this.markAsRead = this.markAsRead.bind(this);
+    }
+
+    shouldComponentUpdate(nextProps) {
+        return nextProps.length > this.props.listNotifications.length;
+    }
+
+    markAsRead(id) {
+        console.log(`Notification ${id} has been marked as read`);
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                {this.props.displayDrawer ? (
+                    <div className={css(styles['flex-area'])}>
+                        <div className={css(styles.menuItem)}>
+                            <p>Your notifications</p>
+                        </div>
+                        <div className={css(styles.Notifications)}>
+                            <ul>
+                                {this.props.listNotifications &&
+                                    this.props.listNotifications.length > 0 ? (
+                                    this.props.listNotifications.map(({ id, html, type, value }) => (
+                                        <NotificationItem
+                                            key={id}
+                                            markAsRead={this.markAsRead}
+                                            type={type}
+                                            value={value}
+                                            html={html}
+                                        />
+                                    ))
+                                ) : (
+                                    <div className={css(styles['notification-header'])}>
+                                        <NotificationItem value="No new notification for now" />
+                                        <button
+                                            style={{
+                                                border: 'none',
+                                                background: 'none',
+                                            }}
+                                            aria-label="Close"
+                                            onClick={() => console.log('Close button has been clicked')}
+                                        >
+                                            <img
+                                                style={{ display: 'inline' }}
+                                                src={closeIcon}
+                                                alt="Close"
+                                            />
+                                        </button>
+                                    </div>
+                                )}
+                            </ul>
+                        </div>
+                    </div>
+                ) : (
+                    <div className={css(styles.menuItem)}>
+                        <p>Your notifications</p>
+                    </div>
+                )}
+            </React.Fragment>
+        );
+    }
+}
+
+Notifications.propTypes = {
+    displayDrawer: PropTypes.bool,
+    listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+Notifications.defaultProps = {
+    displayDrawer: false,
+    listNotifications: [],
+};
+
+export default Notifications;
+ 
